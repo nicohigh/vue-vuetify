2b661d196acd48b3793a9da31ea4acf236f08ae7
nicohigh <nferreyra@mobydigital.com>
2019-03-12 17:25:06 -0300

    se sube por primera vez

---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="home">
+      <h1>Homepage</h1>
+      <v-btn class="pink white--text">Click me</v-btn>
+    </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
